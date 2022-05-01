e44cbafa971179f936df9fd381eeb0310796a686
maulik <maulikpandya1@yahoo.co.in>
2022-05-01 17:00:58 +0530

    Add files via upload
    add run

---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+
+def addition(a,b):
+	return a+b
+	
+def substraction(a,b):
+	return a-b
+	
+def multiplication(a,b):
+	return a*b
+	
+def division(a,b):
+	return a/b
+	
+	
+if __name__=='__main__':
+	a,b=10,2
+	print(addition(a,b))
+	print(substraction(a,b))
+	print(multiplication(a,b))
+	print(division(a,b))
